731cc329cf03d91b9db98cdb6825172e6c3f0377
doyougnu <jmy6342@gmail.com>
2017-04-28 18:16:53 -0700

    got logging to work

--- src/Lang.hs
+++ src/Lang.hs
@@ -2,13 +2,14 @@ module Lang where
 
 import Data.Map as M
 import Control.Monad.State
+import Control.Monad.Writer
+import Control.Applicative
 
 type Var = String
 
 -- Vocabulary
 data Lit = LBool Bool
          | LInt Int
-         deriving (Show)
 
 data Expr = L Lit
           | V Var
@@ -19,66 +20,79 @@ data Expr = L Lit
           | Seq Expr Expr
           | While Expr Expr
           | NoOp
-          deriving (Show)
 
-type GState a = StateT (M.Map Var a) (Maybe) a
+-- Types
+type StateV = M.Map Var Expr
+type Log = String
+type StateM = WriterT Log (StateT (StateV) (Maybe)) Expr
 
 -- Testing
-emptyState :: M.Map var Expr
+emptyState :: StateV
 emptyState = M.empty
 
-emptyStateM :: GState Expr
-emptyStateM = return NoOp
-
-testState :: GState Expr
-testState = do
-  put $ M.fromList [("x", L (LInt 0)), ("y", L (LInt 10))] 
-  return NoOp
+testState :: StateV
+testState = M.fromList [("x", L (LInt 0)), ("y", L (LInt 10))]
 
 x = While (Not (Equal (V "x") (V "y"))) (Let "x" (Add (V "x") (L $ LInt 1)))
-testx = runStateT (eval x) $ M.fromList [("x", L (LInt 0)), ("y", L (LInt 10))]
 
 -- Denotational Semantics
-eval :: Expr -> GState Expr
+eval :: Expr -> StateM
 eval e@(L _) = return e
 eval (V var) = do
   st <- get
-  val <- lift . M.lookup var $ st
+  val <- lift . lift . M.lookup var $ st
   return val
 eval (Not e) = do
   (L (LBool b)) <- eval e
   return . L. LBool $ not b
-eval (Add l r) = do
+eval e@(Add l r) = do
   (L (LInt l')) <- eval l
   (L (LInt r')) <- eval r
-  return . L . LInt $ (l' + r')
+  let res = l' + r'
+  tell $ show e ++ " : " ++ show res ++ "\n"
+  return . L . LInt $ res
 eval (Let var exp) = do
   st <- get
   e <- eval exp
-  let newst =  M.insert var e st
-  put newst
+  put $ M.insert var e st
   return NoOp
 eval (Seq one two) = do
   eval one
   eval two
-eval (Equal l r) = do
-  l' <- eval l
-  r' <- eval r
-  -- This is ugly and basically non monadic
-  case l' of
-    (L (LInt l'')) -> case r' of
-      (L (LInt r'')) -> return . L . LBool $ (l'' == r'')
-      _              -> return NoOp
-    (L (LBool l''))-> case r' of
-      (L (LBool r''))-> return . L . LBool $ (l'' == r'')
-      _              -> return NoOp
-
+eval (Equal l r) = ints <|> bools
+  where ints = do
+          (L (LInt l')) <- eval l
+          (L (LInt r')) <- eval r
+          return . L. LBool $ l' == r'
+        bools = do
+          (L (LBool l')) <- eval l
+          (L (LBool r')) <- eval r
+          return . L. LBool $ l' == r'
 eval e@(While cond body) = do
   (L (LBool cond')) <- eval cond
   if cond'
-    then
-    do
+    then do
     eval body
     eval e
     else return NoOp
 eval e = return e
+
+-- Running the monad stack
+runLang :: Expr -> StateV -> Maybe ((Expr, Log), StateV)
+runLang e s = runStateT (runWriterT $ eval e) s
+
+-- Pretty Printing instance
+instance Show Lit where
+  show (LBool b) = show b
+  show (LInt i)  = show i
+
+instance Show Expr where
+  show (L l) = show l
+  show (V v) = show v
+  show (Add e e') = show e ++ " + " ++ show e'
+  show (Not e) = "~" ++ show e
+  show (Equal e e') = show e ++ " == " ++ show e'
+  show (Let v e) = show v ++ " = " ++ show e
+  show (Seq e e') = show e ++ "; " ++ show e'
+  show (While e e') = "while (" ++ show e ++ ")" ++ "{\n" ++ show e' ++ "}\n"
+  show NoOp = ""
