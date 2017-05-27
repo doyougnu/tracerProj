45c185fa72c0827f3f7425ab418fcc090ec6495d
doyougnu <jmy6342@gmail.com>
2017-05-27 15:19:17 -0700

    control flow done

--- src/CFG.hs
+++ src/CFG.hs
@@ -2,7 +2,8 @@ module CFG where
 
 import qualified Data.Map as M
 import qualified Data.Set as S
-import Debug.Trace (trace)
+-- import Control.Arrow
+-- import Debug.Trace (trace)
 
 import Lang
 
@@ -15,7 +16,7 @@ data Etype = Data
            | Control
            deriving Show
 
--- | An Edge is either a data edge or Control edge, if data the 
+-- | An Edge is either a data edge or Control edge
 data Edge = Edge Etype Node Node 
   deriving Show
 
@@ -41,72 +42,97 @@ instance Graph DGraph Node Edge where
 instance Monoid (DGraph Node Edge) where
   mempty = DGraph S.empty M.empty
   mappend (DGraph ns es) (DGraph ns' es') =
-    DGraph (S.union ns ns') (M.union es es')
+    DGraph (S.union ns ns') (M.unionWith (++) es es')
 
 -- | Given a variable and a statement, return bool if that variable is ever
 -- needed inside the statement
 mentions :: Var -> Stmt -> Bool
 mentions str (BL (RBinary _ (V s1) (V s2))) = s1 == str || s2 == str
-mentions str (AR (V s)) = str == s
-mentions str (AR (ABinary _ a1 a2)) = mentions str (AR a1) || mentions str (AR a2)
-mentions str (Let _ s) = mentions str s
-mentions str (If c t e) = mentions str (BL c) || mentions str t || mentions str e
-mentions str (While c e) = mentions str (BL c) || mentions str e
-mentions str (Seq xs) = any (mentions str) xs
-mentions _   _        = False
+mentions str (AR (V s))                     = str == s
+mentions str (AR (ABinary _ a1 a2))         = mentions str (AR a1)
+                                              || mentions str (AR a2)
+
+mentions str (Let _ s)                      = mentions str s
+mentions str (If c t e)                     = mentions str (BL c)
+                                              || mentions str t
+                                              || mentions str e
+
+mentions str (While c e)                    = mentions str (BL c)
+                                              || mentions str e
+
+mentions str (Seq xs)                       = any (mentions str) xs
+mentions _   _                              = False
 
 -- | Given a statement, return all variables that are referenced inside that
 -- statement
 allVars :: Stmt -> S.Set Var
 allVars (BL (RBinary _ (V s1) (V s2))) = S.fromList [s1, s2]
-allVars (BL (Not b)) = allVars (BL b)
-allVars (BL (BBinary _ b1 b2)) = S.unions [allVars (BL b1), allVars (BL b2)]
-allVars (AR (V s)) = S.singleton s
-allVars (AR (ABinary _ a1 a2)) = S.unions [allVars (AR a1), allVars (AR a2)]
-allVars (Let _ s) = allVars s
-allVars (If c t e) = S.unions [allVars (BL c), allVars t, allVars e]
-allVars (While c e) = S.unions [allVars (BL c), allVars e]
-allVars (Seq xs) =  S.unions $ fmap allVars xs
-allVars _        = S.empty
+allVars (BL (Not b))                   = allVars (BL b)
+allVars (BL (BBinary _ b1 b2))         = S.unions [ allVars (BL b1)
+                                                  , allVars (BL b2)
+                                                  ]
+allVars (AR (V s))                     = S.singleton s
+allVars (AR (ABinary _ a1 a2))         = S.unions [ allVars (AR a1)
+                                                  , allVars (AR a2)
+                                                  ]
+allVars (Let _ s)                      = allVars s
+allVars (If c t e)                     = S.unions [ allVars (BL c)
+                                                  , allVars t
+                                                  , allVars e
+                                                  ]
+allVars (While c e)                    = S.unions [ allVars (BL c)
+                                                  , allVars e
+                                                  ]
+allVars (Seq xs)                       = S.unions $ fmap allVars xs
+allVars _                              = mempty
 
+-- | Given a Statement, pack the nodes in a graph with those statments
 packNodes :: Stmt -> DGraph Node Edge -> DGraph Node Edge
-packNodes a@(Let var s) g = addNode (var, a) g
+packNodes a@(Let var _) g = addNode (var, a) g
 packNodes (Seq ss)      g = mconcat $ flip packNodes g <$> ss
 packNodes a             g = addNode ("", a) g
 
+-- | Given a statement, and a graph, add data edges to that graph
 toDataDCFG :: Stmt -> DGraph Node Edge -> DGraph Node Edge
-toDataDCFG s@(Let var s')  g = dataHelper [var] ns Data g''
+toDataDCFG s@(Let var s')  g = mconcat $ helper <$> vars <*> ns
   where
-    newNode = (var, s)
-    g' = addNode newNode g
-    -- special case for let
-    g'' = if mentions var s'
-          then addEdge newNode (Edge Data newNode newNode) g'
-          else g'
-    ns = nodes g''
-    dataHelper a@(x:_) (y:ys) etype gr
-      | trace (show a) $ mentions x (snd y) = addEdge y (Edge etype y ("", s)) gr
-      | otherwise = dataHelper a ys etype gr
-    dataHelper (_:xs) [] etype gr = dataHelper xs ns etype gr
-    dataHelper []     _  _     gr = gr
+    ns = nodes g
+    vars = S.toList $ allVars s'
+    helper x y = if x == fst y
+               then addEdge y (Edge Data y (var, s)) g
+               else g
 
-toDataDCFG s@(If c t e)    g = dataHelper vars ns Data g
-  where vars = S.toList $ S.unions [allVars (BL c), allVars t, allVars e]
-        ns   = nodes g 
-        dataHelper a@(x:_) (y:ys) etype gr
-          | x == fst y = addEdge y (Edge etype y ("", s)) gr
-          | otherwise = dataHelper a ys etype gr
-        dataHelper (_:xs) [] etype gr = dataHelper xs ns etype gr
-        dataHelper []     _  _     gr = gr
+toDataDCFG s@(If c t e) g = mconcat $ toDataDCFG (BL c) g :
+                            toDataDCFG t g :
+                            toDataDCFG e g :
+                            (helper <$> vars <*> ns) 
+  where
+    ns = nodes g
+    vars = S.toList . S.unions $ [allVars (BL c), allVars t, allVars e]
+    helper x y = if x == fst y
+               then addEdge y (Edge Data y ("", s)) g
+               else g
 
-toDataDCFG s@(While b e)   g = dataHelper vars ns Data g
-  where vars = S.toList $ S.unions [allVars (BL b), allVars e]
-        ns = nodes g
-        dataHelper a@(x:_) (y:ys) etype gr
-          | x == fst y = addEdge y (Edge etype y ("", s)) gr
-          | otherwise = dataHelper a ys etype gr
-        dataHelper (_:xs) [] etype gr = dataHelper xs ns etype gr
-        dataHelper []     _  _     gr = gr
+toDataDCFG s@(While b e) g = mconcat $ toDataDCFG (BL b) g :
+                             toDataDCFG e g :
+                             (helper <$> vars <*> ns)
+  where
+    ns = nodes g
+    vars = S.toList . S.unions $ [allVars (BL b), allVars e]
+    helper x y = if x == fst y
+               then addEdge y (Edge Data y ("", s)) g
+               else g
 toDataDCFG (Seq ss) g = mconcat $ flip toDataDCFG g <$> ss
--- toDataDCFG (Seq ss) g = foldr toDataDCFG g ss  
 toDataDCFG _        g = g
+
+-- | Given a statement, and a graph, add control flow edges
+toDataCCFG :: Stmt -> DGraph Node Edge -> DGraph Node Edge
+toDataCCFG s@(If c t e)  g = addEdge tNode (Edge Control sNode tNode) g `mappend`
+                             addEdge eNode (Edge Control sNode eNode) g
+  where tNode = ("", t)
+        eNode = ("", e)
+        sNode = ("", s)
+toDataCCFG s@(While b e) g = addEdge eNode (Edge Control sNode eNode) g
+  where sNode = ("", s)
+        eNode = ("", e)
+toDataCCFG _             g = g
